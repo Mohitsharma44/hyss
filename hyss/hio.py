add2081e7c8a53f2c102bee609bc91a46af4d79d
Greg Dobler <greg.dobler@gmail.com>
2015-01-22 01:51:41 -0500

    moving away from environment variables in favor of configuration files

--- hyss/hio.py
+++ hyss/hio.py
@@ -1,8 +1,68 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# -------- 
+#  HYSS I/O utilities.
+#
+#  2015/01/22 - Written by Greg Dobler (CUSP/NYU)
+# -------- 
+
 import os
 import numpy as np
+
+
+# -------- 
+#  set configuration
+# -------- 
+def load_config(infile):
+    """
+    Load a configuration file.
+
+    The configuration file must be of the format,
+
+    # Comments line 1
+    # Comments line 2
+    HYSS_DPATH : /path/to/data/file
+    HYSS_DNAME : name_of_data_file
+    HYSS_HPATH : /path/to/header/file
+    HYSS_HNAME : name_of_header_file
+    HYSS_WRITE : /path/to/write/output
+    NOAA_DATA  : /path/to/noaa/data
+
+    Paramters
+    ---------
+    infile : str
+        The name of the configuration file to load.
+    """
+
+    # -- Update the config dictionary
+    for line in open(infile,'r'):
+        if line[0]=='#':
+            continue
+        elif ':' in line:
+            recs = line.split(':')
+            HYSS_ENVIRON[recs[0].replace(" ","")] = \
+                recs[1].replace("\n","").lstrip().rstrip()
+
+    return
+
+# -- hold default paths and load a config file if it exists
+HYSS_ENVIRON = {'HYSS_DPATH' : '.',
+                'HYSS_DNAME' : '.',
+                'HYSS_HPATH' : '.',
+                'HYSS_HNAME' : '.',
+                'HYSS_WRITE' : '.',
+                'NOAA_DATA'  : '.'
+                }
+
+config_files = [i for i in os.listdir('.') if i.endswith('.hcfg')]
+
+if len(config_files)==1:
+    print('HIO: found configuration file {0}.'.format(config_files[0]))
+    print('HIO: initializing configuration, check hyss.HYSS_ENVIRON for ' 
+          'details.')
+    load_config(config_files[0])
+
 
 
 class HyperHeader():
