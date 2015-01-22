e72d903954c5f20c3345947551fb46fa43f21e0b
Greg Dobler <greg.dobler@gmail.com>
2015-01-22 12:00:25 -0500

    include configuration

--- hyss/__init__.py
+++ hyss/__init__.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# -------- 
+#  set configuration
+# -------- 
+from .config import *
+
+HYSS_ENVIRON = default_environ()
+
+# -------- 
+#  import hyss
+# -------- 
 from .hio import *
+from .hyperheader import *
 from .hypercube import *
 from .reduce import *
+
+
