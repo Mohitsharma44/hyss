a947b18178a5366facc587cfe110e3c64a890bdf
greg.dobler@gmail.com <greg.dobler@gmail.com>
2016-04-03 22:01:05 -0400

    tap cluster figure

--- hyss/vnir_make_figures.py
+++ hyss/vnir_make_figures.py
@@ -594,110 +594,146 @@ def plot_img(data, rgb=False, aspect=0.45, cmap="bone", clim=None, title="",
 #fig.savefig("../output/km_cluster_noaa_comp.eps", clobber=True)
 
 
-##############################
-# Lighting technology labels #
-##############################
-
-# -- the technologies are 
-#    1,8,**10**,13,14 (number 1)
-#    2 (number 2)
-#    3,12 (number 3)
-#    4, **5** (number 4)
-#    0 (number 5)
-#    7 (number 6)
-
-# -- load the K-Means clusters
-km    = pkl.load(open("../output/km_cluster.pkl"))
-waves = np.load('../output/vnir_waves.npy')
-
-# -- load the integrated image
-img_L = np.load("../output/img_L_arr.npy")
-ind   = img_L > 0.5
-ind  = (gf(1.0*ind,1)>0.25)*ind
-
-# -- set the colors, types, and KM indices
-clrs  = ['#E24A33','#8EBA42','#348ABD','#988ED5','#FBC15E','#FFB5B8']
-types = ['High\nPressure\nSodium','LED','Fluorescent','Fluorescent',
-         'Metal\nHalide','LED','LED']
-kinds = [np.array([10,1,8,14]), np.array([2]), np.array([3,12]),
-         np.array([5,4]), np.array([0]), np.array([7])]
-
-# -- get the x/y positions of all active pixels
-pos      = np.arange(img_L.size).reshape(img_L.shape)[ind]
-xpos_all = pos % img_L.shape[1]
-ypos_all = pos // img_L.shape[1]
-
-# -- get the positions for each type
-xpos = []
-ypos = []
-for ii in range(len(kinds)):
-    txpos = np.hstack([xpos_all[km.labels_==kind] for kind in kinds[ii]])
-    typos = np.hstack([ypos_all[km.labels_==kind] for kind in kinds[ii]])
-    xpos.append(txpos)
-    ypos.append(typos)
-
-# -- plot utilities
-stamp   = img_L[600:850,:250]
-xs      = 1.0*6.5
-asp     = 0.45
-xoff    = 0.05
-wid_top = 1.0 - 2.0*xoff
-wid_bot = (wid_top - 0.5*xoff)*0.5
-rat_top = 1600./1560.
-rat_bot = 250./250.
-ys      = 2.5*xoff*xs + wid_top*rat_top*asp*xs + wid_bot*rat_bot*asp*xs
-hgt_top = wid_top*rat_top*asp*xs/ys
-ysep    = 0.5*xoff*xs/ys
-yoff    = xoff*xs/ys
-hgt_bot = wid_bot*rat_bot*asp*xs/ys
-
-# -- initialize the plot
-plt.close("all")
-fig     = plt.figure(figsize=(xs,ys))
-ax_top  = fig.add_axes((xoff,yoff+0.5*yoff+hgt_bot,wid_top,hgt_top))
-ax_botl = fig.add_axes((xoff,yoff,wid_bot,hgt_bot))
-ax_botr = fig.add_axes((0.5+0.25*xoff,yoff,wid_bot,hgt_bot))
-
-# -- add the lighting tags
-for ii in range(len(kinds)):
-    ax_top.scatter(xpos[ii],ypos[ii],2,clrs[ii],"s",lw=0)
-    ax_botr.scatter(xpos[ii],ypos[ii],5,clrs[ii],"s",lw=0)
-im_top = ax_top.imshow(img_L,"bone",clim=[0,3],aspect=0.45)
-im_bot = ax_botr.imshow(img_L,"bone",clim=[0,3],aspect=0.45)
-ax_top.axis("off")
-ax_botr.axis("off")
-ax_botr.set_xlim(0,250)
-ax_botr.set_ylim(850,600)
-
-# -- label the top and bottom images
-yr = ax_top.get_ylim()
-xr = ax_top.get_xlim()
-ax_top.text(xr[0],yr[1]-0.03*(yr[0]-yr[1]),
-            "New York City Lighting Technologies", ha="left", va="center", 
-            fontsize=txtsz)
-ax_botr.text(250, 850+0.08*(850-600), "Manhattan Bridge Region", 
-             ha="right", va="center", fontsize=txtsz)
-
-# -- plot the spectra
-ax_botl.set_ylim(-1,8)
-ax_botl.set_xlim(0,6*(waves[-1]-waves[0]))
-for ii in range(6):
-    twaves = waves-waves[0]+ii*(waves[-1]-waves[0])
-    tkmc   = km.cluster_centers_[kinds[ii][0]]
-    ax_botl.plot(twaves, tkmc-tkmc.min(), color=clrs[ii],lw=0.5)
-    ax_botl.text(0.5*(twaves.max()+twaves.min()),7.9,types[ii],fontsize=5,
-                 ha="center",va="top")
-
-ax_botl.set_xticks([ii*(waves[-1]-waves[0]) for ii in range(6)])
-ax_botl.set_xticklabels("")
-ax_botl.set_yticklabels("")
-ax_botl.xaxis.grid(1)
-ax_botl.set_ylabel("intensity [arb units]")
-ax_botl.set_xlabel("wavelength [range: 0.4-1.0 micron]")
-fig.canvas.draw()
-fig.savefig('../output/spectral_class_map.eps', clobber=True)
+###############################
+## Lighting technology labels #
+###############################
+#
+## -- the technologies are 
+##    1,8,**10**,13,14 (number 1)
+##    2 (number 2)
+##    3,12 (number 3)
+##    4, **5** (number 4)
+##    0 (number 5)
+##    7 (number 6)
+#
+## -- load the K-Means clusters
+#km    = pkl.load(open("../output/km_cluster.pkl"))
+#waves = np.load('../output/vnir_waves.npy')
+#
+## -- load the integrated image
+#img_L = np.load("../output/img_L_arr.npy")
+#ind   = img_L > 0.5
+#ind  = (gf(1.0*ind,1)>0.25)*ind
+#
+## -- set the colors, types, and KM indices
+#clrs  = ['#E24A33','#8EBA42','#348ABD','#988ED5','#FBC15E','#FFB5B8']
+#types = ['High\nPressure\nSodium','LED','Fluorescent','Fluorescent',
+#         'Metal\nHalide','LED','LED']
+#kinds = [np.array([10,1,8,14]), np.array([2]), np.array([3,12]),
+#         np.array([5,4]), np.array([0]), np.array([7])]
+#
+## -- get the x/y positions of all active pixels
+#pos      = np.arange(img_L.size).reshape(img_L.shape)[ind]
+#xpos_all = pos % img_L.shape[1]
+#ypos_all = pos // img_L.shape[1]
+#
+## -- get the positions for each type
+#xpos = []
+#ypos = []
+#for ii in range(len(kinds)):
+#    txpos = np.hstack([xpos_all[km.labels_==kind] for kind in kinds[ii]])
+#    typos = np.hstack([ypos_all[km.labels_==kind] for kind in kinds[ii]])
+#    xpos.append(txpos)
+#    ypos.append(typos)
+#
+## -- plot utilities
+#stamp   = img_L[600:850,:250]
+#xs      = 1.0*6.5
+#asp     = 0.45
+#xoff    = 0.05
+#wid_top = 1.0 - 2.0*xoff
+#wid_bot = (wid_top - 0.5*xoff)*0.5
+#rat_top = 1600./1560.
+#rat_bot = 250./250.
+#ys      = 2.5*xoff*xs + wid_top*rat_top*asp*xs + wid_bot*rat_bot*asp*xs
+#hgt_top = wid_top*rat_top*asp*xs/ys
+#ysep    = 0.5*xoff*xs/ys
+#yoff    = xoff*xs/ys
+#hgt_bot = wid_bot*rat_bot*asp*xs/ys
+#
+## -- initialize the plot
+#plt.close("all")
+#fig     = plt.figure(figsize=(xs,ys))
+#ax_top  = fig.add_axes((xoff,yoff+0.5*yoff+hgt_bot,wid_top,hgt_top))
+#ax_botl = fig.add_axes((xoff,yoff,wid_bot,hgt_bot))
+#ax_botr = fig.add_axes((0.5+0.25*xoff,yoff,wid_bot,hgt_bot))
+#
+## -- add the lighting tags
+#for ii in range(len(kinds)):
+#    ax_top.scatter(xpos[ii],ypos[ii],2,clrs[ii],"s",lw=0)
+#    ax_botr.scatter(xpos[ii],ypos[ii],5,clrs[ii],"s",lw=0)
+#im_top = ax_top.imshow(img_L,"bone",clim=[0,3],aspect=0.45)
+#im_bot = ax_botr.imshow(img_L,"bone",clim=[0,3],aspect=0.45)
+#ax_top.axis("off")
+#ax_botr.axis("off")
+#ax_botr.set_xlim(0,250)
+#ax_botr.set_ylim(850,600)
+#
+## -- label the top and bottom images
+#yr = ax_top.get_ylim()
+#xr = ax_top.get_xlim()
+#ax_top.text(xr[0],yr[1]-0.03*(yr[0]-yr[1]),
+#            "New York City Lighting Technologies", ha="left", va="center", 
+#            fontsize=txtsz)
+#ax_botr.text(250, 850+0.08*(850-600), "Manhattan Bridge Region", 
+#             ha="right", va="center", fontsize=txtsz)
+#
+## -- plot the spectra
+#ax_botl.set_ylim(-1,8)
+#ax_botl.set_xlim(0,6*(waves[-1]-waves[0]))
+#for ii in range(6):
+#    twaves = waves-waves[0]+ii*(waves[-1]-waves[0])
+#    tkmc   = km.cluster_centers_[kinds[ii][0]]
+#    ax_botl.plot(twaves, tkmc-tkmc.min(), color=clrs[ii],lw=0.5)
+#    ax_botl.text(0.5*(twaves.max()+twaves.min()),7.9,types[ii],fontsize=5,
+#                 ha="center",va="top")
+#
+#ax_botl.set_xticks([ii*(waves[-1]-waves[0]) for ii in range(6)])
+#ax_botl.set_xticklabels("")
+#ax_botl.set_yticklabels("")
+#ax_botl.xaxis.grid(1)
+#ax_botl.set_ylabel("intensity [arb units]")
+#ax_botl.set_xlabel("wavelength [range: 0.4-1.0 micron]")
+#fig.canvas.draw()
+#fig.savefig('../output/spectral_class_map.eps', clobber=True)
 
 
 #######################
 # Full TAP clustering #
 #######################
+
+# -- load the tap clusters
+print("reading tap clusters...")
+waves = np.load("../output/vnir_waves.npy")*1e-3
+taps  = np.load("../output/tap_cl_fil.npy")
+
+# -- normalize taps
+print("normalizing tap clusters...")
+taps -= taps.min(1,keepdims=True)
+taps *= 0.9/taps.max(1,keepdims=True)
+
+# -- set up the plot
+xs = 6.5
+ys = xs*5.0/7.0
+fig, ax = plt.subplots(6,7,figsize=(xs,ys), sharex=True, sharey=True)
+fig.subplots_adjust(0.075,0.1,0.95,0.95,0.3)
+
+# -- loop through and plot
+print("looping through taps and plotting...")
+for itap, tap in enumerate(taps):
+    irow = itap//7
+    icol = itap%7
+    ax[irow,icol].plot(waves,tap,color="darkred")
+    ax[irow,icol].grid(1)
+    ax[irow,icol].set_xlim(waves[0],waves[-1])
+    ax[irow,icol].set_ylim(-0.1,1)
+    ax[irow,icol].set_frame_on(False)
+    ax[irow,icol].set_yticklabels("")
+    ax[irow,icol].set_xticks([0.4,0.6,0.8,1.0])
+    ax[irow,icol].set_xticklabels([0.4,0.6,0.8,1.0],rotation=45,ha="right")
+    ax[irow,icol].tick_params(labelsize=8,)
+fig.text(0.03,0.5,"intensity [arb units]",fontsize=txtsz,rotation=90,
+         va="center")
+fig.text(0.5,0.02,"wavelength [micron]",fontsize=txtsz,ha="center")
+fig.canvas.draw()
+fig.savefig("../output/unique_tap_clusters.eps",clobber=True)
